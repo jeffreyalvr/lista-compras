73684569fe1c1c510dc4b310a8e92411639a51a1
Jeffrey Ramo <matheusunitt@gmail.com>
2023-04-27 14:12:12 -0300

    Altera o height do Header de acordo com o tamanho da tela

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="h-60 border-b border-b-[#dadada] p-30 flex items-center justify-center">
+    <header className="h-[8rem] sm:h-[15rem] border-b border-b-[#dadada] p-30 flex items-center justify-center">
       <h1 className="text-4xl font-bold text-black">Lista de compras</h1>
     </header>
   );
