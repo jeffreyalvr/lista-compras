3579cb2fd5a2cf80b17b8b17f371cfe7f42035e3
Jeffrey Ramo <matheusunitt@gmail.com>
2023-04-27 14:45:51 -0300

    Altera um pouco o design para dispositivos menores

--- src/components/Box/index.jsx
+++ src/components/Box/index.jsx
@@ -150,20 +150,22 @@ const Box = () => {
         ) : (
           lista.map((item) => (
             <div
-              className="py-4 border-dotted border-b border-[#dadada] flex justify-between"
+              className="py-4 border-dotted border-b border-[#dadada] flex flex-col sm:flex-row justify-between"
               key={item.id}
             >
-              <div className="flex overflow-hidden">{item.nome}</div>
-              <div className="flex justify-between max-w-[140px]">
+              <div className="flex overflow-hidden py-4 sm:py-0">
+                {item.nome}
+              </div>
+              <div className="flex md:max-w-[140px]">
                 <button
                   onClick={() => handleEditarItem(item.id)}
-                  className="bg-[#4bc9ff] outline-1 text-xs font-bold	hover:bg-[#6cd3ff] text-white rounded-[1.25rem] py-[0.625rem] px-[1.25rem] mx-1 w-auto box-content align-middle"
+                  className="bg-[#4bc9ff] outline-1 text-xs font-bold hover:bg-[#6cd3ff] w-[50%] text-white rounded-[1.25rem] py-[0.625rem] px-[1.25rem] mx-1 box-content flex flex-col justify-content items-center"
                 >
                   <img src={icon_edit} alt="editar" title="editar" />
                 </button>
                 <button
                   onClick={() => handleRemoverItem(item.id)}
-                  className="bg-[#ff4b4b] outline-1 text-xs font-bold	hover:bg-[#ff6262] text-white rounded-[1.25rem] py-[0.625rem] px-[1.25rem] mx-1 w-auto box-content align-middle"
+                  className="bg-[#ff4b4b] outline-1 text-xs font-bold hover:bg-[#ff6262] w-[50%] text-white rounded-[1.25rem] py-[0.625rem] px-[1.25rem] mx-1 box-content flex flex-col justify-content items-center"
                 >
                   <img src={icon_delete} alt="excluir" title="excluir" />
                 </button>
